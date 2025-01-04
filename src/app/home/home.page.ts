a32cc3b5151605e936c8e0ef01c4495ddb71e668
G00438832 <g00438832@atu.ie>
2025-01-04 22:49:16 +0000

    Aligned toolbar horizontally

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons } from '@ionic/angular/standalone';
 import { settingsOutline } from 'ionicons/icons';
 import {addIcons} from "ionicons";
 
@@ -7,7 +7,7 @@ import {addIcons} from "ionicons";
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons],
 })
 export class HomePage {
   constructor() {
