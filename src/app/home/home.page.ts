a874f8e96af821b8b7be0dd04105a765fdfc9729
G00438832 <g00438832@atu.ie>
2025-01-04 22:45:27 +0000

    Adding settings button and title

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { settingsOutline } from 'ionicons/icons';
+import {addIcons} from "ionicons";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ settingsOutline });
+  }
 }
