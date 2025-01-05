785c77ce7ed7d594821dcb3683e3e86babcd4401
G00438832 <g00438832@atu.ie>
2025-01-05 15:47:13 +0000

    Tried to get radio buttons to update from stored memory but failed

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonCheckbox, IonItem, IonRadio, IonRadioGroup } from '@ionic/angular/standalone';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-settings',
@@ -10,13 +11,36 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonCheckbox, IonI
   imports: [IonRadioGroup, IonRadio, IonItem, IonCheckbox, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class SettingsPage implements OnInit {
+  selectedValue: string = "Metric";
 
-  constructor() { }
+  constructor(private ds: DataService) { }
 
   ngOnInit() {
   }
 
+  ionViewWillEnter(){
+    console.log(this.getValue);
+    if(this.ds.get("Unit") == null){
+      this.selectedValue = "Metric";
+      this.ds.set("Unit", this.selectedValue);
+    }
+    else{
+      this.setValue();
+    }
+
+    document.getElementById("radioGroup")?.setAttribute("value", this.selectedValue);
+  }
+
+  async getValue(){
+    return await this.ds.get("Unit")
+  }
+
+  async setValue(){
+    this.selectedValue = await this.ds.get("Unit");
+  }
+
   unitChange(value: string){
-    console.log(value);
+    this.selectedValue = value;
+    this.ds.set("Unit", this.selectedValue);
   }
 }
