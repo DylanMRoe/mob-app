3e5407b790473d28a9f1f78134dc8e1c3d55dca4
G00438832 <g00438832@atu.ie>
2025-01-05 14:57:24 +0000

    Fixing radiobutton events

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonCheckbox, IonItem, IonRadio, IonRadioGroup } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonRadioGroup, IonRadio, IonItem, IonCheckbox, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class SettingsPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  unitChange(){
+    console.log("Working");
   }
 
 }
