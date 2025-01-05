9b2825ba63bed5b93bf8234303ebe2aa7b8a055e
G00438832 <g00438832@atu.ie>
2025-01-05 11:24:08 +0000

    Set up to try to delete update-homepg branch

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, IonInput } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons} from '@ionic/angular/standalone';
 import { settingsOutline } from 'ionicons/icons';
 import {addIcons} from "ionicons";
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, IonInput, FormsModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons],
 })
 export class HomePage {
   searchName: string = "";
