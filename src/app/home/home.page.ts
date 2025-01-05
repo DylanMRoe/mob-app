1c05d831e944a0bf0c102af684f1016f7e81e6a5
G00438832 <g00438832@atu.ie>
2025-01-05 11:14:46 +0000

    Updated home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, IonInput } from '@ionic/angular/standalone';
 import { settingsOutline } from 'ionicons/icons';
 import {addIcons} from "ionicons";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonButtons, IonInput, FormsModule],
 })
 export class HomePage {
+  searchName: string = "";
+
   constructor() {
     addIcons({ settingsOutline });
   }
